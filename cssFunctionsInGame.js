b376ce81567d69c4675184f78491be9d4a36e007
Daniel Lepszy <daniel.lepszy@gmail.com>
2018-02-01 14:37:42 +0100

    Add more information about game

--- cssFunctionsInGame.js
+++ cssFunctionsInGame.js
@@ -17,7 +17,7 @@ function gameInformation() {
         'Zasady gry są proste: grasz dopóki nie wygrasz :).<br>Można korzystać tylko z jednego koła ratunkowego podczas danego pytania.<br>' +
         'Pamiętaj, że pytania z pozoru wydają się łatwe.<br><br> Powodzenia ! <br><br>Prawa autorskie:<br>'+ 
         '- Pliki MP3 (http://www.kakofonia.pl/PLtele.php)<br>'+
-        '- Pliki PNG (s)');
+        '- Pliki PNG (http://www.studios40.fr/news/?id=52)');
 
     $('#startGame').addClass('gameInformationsStyles');
 }
