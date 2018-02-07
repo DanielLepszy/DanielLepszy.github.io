b1a3ce75043028eb3330f96078d6c0c3b84083de
Daniel Lepszy <daniel.lepszy@gmail.com>
2018-02-07 12:19:23 +0100

    Fix

--- audio.js
+++ audio.js
@@ -1,37 +1,36 @@
-var focusMusicPlayer = null
+var focusMusicPlayer = new Audio('Sounds/FocusSound.mp3');
+focusMusicPlayer.loop = true
+
 
 function playQuestionFocusMusic() {
-    focusMusicPlayer = new Howl({
-        src: ['./Sounds/FocusSound.mp3'],
-        autoplay: true,
-        loop: true,
-        volume: 1
-    });
     focusMusicPlayer.play()
 };
 
-function playLastQuestionFocusMusic(didEndCallback) {
-    focusMusicPlayer.pause();
-    var focusMusicPlayer = new Howl({
-        src: ['./Sounds/lastFocus.mp3'],
-        autoplay: true,
-        loop: true,
-        volume: 1,
-        onend: didEndCallback
-    });
+function playLastQuestionFocusMusic() {
+    focusMusicPlayer = new Audio('Sounds/lastFocus.mp3')
+    focusMusicPlayer.loop = true
     focusMusicPlayer.play()
 }
 
 function descreaseVolumeQuestionFocusMusic() {
-    focusMusicPlayer.stop();
+    focusMusicPlayer.volume = 0.0;
 }
 
 function increaseVolumeQuestionFocusMusic() {
-    //focusMusicPlayer.volume = 1;
-    focusMusicPlayer.play();
+    focusMusicPlayer.volume = 1;
 }
 
 function playAudio(url, didEndCallback) {
+
+    // var audio = new Audio(url);
+    // audio.loop = false;
+    // audio.play();
+    // audio.onended = didEndCallback
+
+    // createjs.Sound.registerSound(url, "x");
+    // var audio = createjs.Sound.play("x");
+    // audio.on('complete',didEndCallback,this);
+
     var sound = new Howl({
         src: [url],
         autoplay: true,
